9b5514a57d968be9f0ac08df67ec9fd1260f3b0f
LarsPeuling <lpeuling@gmail.com>
2023-12-10 16:57:19 +0100

    Add project files.

---
+++ DatingApplication/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DatingApplication
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
